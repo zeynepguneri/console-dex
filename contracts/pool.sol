b7f61a7075e22497ac75d8c5130d4a0099febec4
Alp <baturalperbay@gmail.com>
2025-01-09 20:36:47 +0300

    genel bir şema, parametre isimlerine ve constructora bilerek ellemiyorum.

---
+++ contracts/pool.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Pool {
+    constructor() {}
+
+    function addLiquidity() external {
+        return;
+    }
+
+    function removeLiquidity() external {
+        return;
+    }
+
+    function swap() external {
+        return;
+    }
+
+    function showPoolBalance() public view {
+        return;
+    }
+}
---
+++ contracts/viewHelper.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract viewHelper {
+    function viewUserBalance() public view {
+        return;
+    }
+
+    function viewPool() public view {
+        return;
+    }
+
+    function viewUserAddresses() public view {
+        return;
+    }
+}
