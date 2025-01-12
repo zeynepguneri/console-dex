21f8cc73023a122e1951116f1998f52fde867dcd
Alp <baturalperbay@gmail.com>
2025-01-12 14:12:20 +0300

    .

--- src/menus/pools_menu.js
+++ src/menus/pools_menu.js
@@ -5,24 +5,31 @@ import PoolMenu from "./pool_menu.js";
 async function PoolsMenu() {
     const pools = await poolService.getPools();
 
+    if (pools.length === 0) {
+        console.log("No pools available.");
+        return;
+    }
+
     const poolsChoices = pools.map((pool) => ({
-      name: `${Object.keys(pool.token_1)[0]} / ${Object.keys(pool.token_2)[0]}`, // tokenA / tokenB formatında
-      value: pool.id, // Seçim değeri olarak pool id'si
+        name: `${pool.token_1} / ${pool.token_2}`, // Format token_1 / token_2
+        value: pool.id, // Pool ID as the choice value
     }));
-    
+
     const { choice } = await inquirer.prompt([
-      {
-        type: "list",
-        name: "choice",
-        message: "Pools Menu",
-        choices: [...poolsChoices, "Return Back"]
-      }
+        {
+            type: "list",
+            name: "choice",
+            message: "Pools Menu",
+            choices: [...poolsChoices, "Return Back"],
+        },
     ]);
-    
+
     if (pools.some((pool) => pool.id === choice)) {
-      await PoolMenu(choice);
-      await PoolsMenu();
-    } else if(choice === "Return Back"){}
+        await PoolMenu(choice);
+        await PoolsMenu();
+    } else if (choice === "Return Back") {
+        console.log("Returning to the previous menu...");
+    }
 }
 
 export default PoolsMenu;
